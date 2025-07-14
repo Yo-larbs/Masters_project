79daf19ade7f575295dc532d6c27a8e11cd31157
Yoofi Larbi <jyalarbi@outlook.com>
2025-07-14 15:25:54 +0100

    -added RNA-seq -added outlier graph visualisations -changed all-freq graph on genomic analyses -added code to find true overlapping variants between REGENIE and PLINK and edited Manhattan plot

--- scripts/Bash scripts/index.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-Docker run --platform linux/amd64 -v ../..:/data quay.io/biocontainers/bcftools:1.7--0 bcftools sort -o "/data/output/filtered_merged_genome.vcf" "/data/output/filtered_merged_genome.vcf"
-Docker run --platform linux/amd64 -v ../..:/data quay.io/biocontainers/bcftools:1.7--0 bcftools convert -O z -o  "/data/output/filtered_merged_genome.vcf.gz" "/data/output/filtered_merged_genome.vcf"
-Docker run --platform linux/amd64 -v ../..:/data quay.io/biocontainers/bcftools:1.7--0 bcftools index  "/data/output/filtered_merged_genome.vcf.gz"
